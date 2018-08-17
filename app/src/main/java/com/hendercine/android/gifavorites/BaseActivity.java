a05119d0404c4f33d22232c33def39d98ccad0ec
hendercine <james@hendercine.com>
2018-08-16 21:53:44 -0700

    feat($java): create BaseActivity
    - Add Timber & utility methods for network checks, toasts & snackbars.

---
+++ app/src/main/java/com/hendercine/android/gifavorites/BaseActivity.java
@@ -0,0 +1,60 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 8/16/18 9:39 PM
+ */
+
+package com.hendercine.android.gifavorites;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import timber.log.Timber;
+
+/**
+ * Gifavorites created by artemis on 8/16/18.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    public void showSnackBar(int resId) {
+        Snackbar.make(
+                findViewById(android.R.id.content),
+                resId,
+                Snackbar.LENGTH_SHORT
+        ).show();
+    }
+
+    public void showToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showToast(int resId) {
+        Toast.makeText(getApplicationContext(), resId, Toast.LENGTH_SHORT).show();
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = null;
+        if (connectivityManager != null) {
+            activeNetwork = connectivityManager.getActiveNetworkInfo();
+        }
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+}
