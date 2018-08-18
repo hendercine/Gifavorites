a67e2766594e8cdbf48cd8b0b1a92773f0073c6a
hendercine <james@hendercine.com>
2018-08-18 11:50:58 -0700

    feat($model): create Gif data class

---
+++ app/src/main/java/com/hendercine/android/gifavorites/model/Gif.kt
@@ -0,0 +1,17 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 8/17/18 9:27 PM
+ */
+
+package com.hendercine.android.gifavorites.model
+
+/**
+ *  Gifavorites created by artemis on 8/17/18.
+ */
+data class Gif(val id: String,
+               val url: String,
+               val caption: String,
+               val title: String)
