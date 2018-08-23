6316e549aec88ee9da26ced14e04f0427367de64
hendercine <james@hendercine.com>
2018-08-22 21:08:52 -0700

    refactor($mainview): change Main back to Java

---
+++ app/src/main/java/com/hendercine/android/gifavorites/view/mainview/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 8/22/18 8:55 PM
+ */
+
+package com.hendercine.android.gifavorites.view.mainview;
+
+import com.hendercine.android.gifavorites.view.base.BaseActivity;
+
+/**
+ * Gifavorites created by artemis on 8/22/18.
+ */
+public class MainActivity extends BaseActivity {
+
+}
