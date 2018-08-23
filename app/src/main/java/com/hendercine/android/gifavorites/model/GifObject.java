84cdb8d87bfd846c28777df18176828bfe4207dd
hendercine <james@hendercine.com>
2018-08-23 14:00:32 -0700

    refactor($model): make GifObject a Parceler POJO

---
+++ app/src/main/java/com/hendercine/android/gifavorites/model/GifObject.java
@@ -0,0 +1,71 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 8/22/18 8:55 PM
+ */
+
+package com.hendercine.android.gifavorites.model;
+
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
+
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+/**
+ * Gifavorites created by artemis on 8/22/18.
+ */
+
+@Parcel(Parcel.Serialization.BEAN)
+public class GifObject {
+
+    @SuppressWarnings("WeakerAccess")
+    @SerializedName("id")
+    String mGifObjectId;
+    @SuppressWarnings("WeakerAccess")
+    @SerializedName("slug")
+    String mGifObjectSlug;
+    @SuppressWarnings("WeakerAccess")
+    @SerializedName("url")
+    String mGifObjecturl;
+    @SuppressWarnings("WeakerAccess")
+    @SerializedName("images")
+    ArrayList<JsonObject> mGiphyImages;
+
+    public GifObject() {
+    }
+
+    public String getGifObjectId() {
+        return mGifObjectId;
+    }
+
+    public void setGifObjectId(String gifObjectId) {
+        mGifObjectId = gifObjectId;
+    }
+
+    public String getGifObjectSlug() {
+        return mGifObjectSlug;
+    }
+
+    public void setGifObjectSlug(String gifObjectSlug) {
+        mGifObjectSlug = gifObjectSlug;
+    }
+
+    public String getGifObjecturl() {
+        return mGifObjecturl;
+    }
+
+    public void setGifObjecturl(String gifObjecturl) {
+        mGifObjecturl = gifObjecturl;
+    }
+
+    public ArrayList<JsonObject> getGiphyImages() {
+        return mGiphyImages;
+    }
+
+    public void setGiphyImages(ArrayList<JsonObject> giphyImages) {
+        mGiphyImages = giphyImages;
+    }
+}
