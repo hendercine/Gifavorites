d14bbe3fe429bfe9c5b17115e8011be41cbe6814
hendercine <james@hendercine.com>
2018-08-22 21:11:29 -0700

    feat($data): create GiphyService component

---
+++ app/src/main/java/com/hendercine/android/gifavorites/data/GiphyService.java
@@ -0,0 +1,28 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 8/22/18 8:53 PM
+ */
+
+package com.hendercine.android.gifavorites.data;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+/**
+ * Gifavorites created by artemis on 8/22/18.
+ */
+public class GiphyService extends IntentService {
+
+    public GiphyService() {
+        super(GiphyService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
