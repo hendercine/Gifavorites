35d4ae15d66741afaf42c8a054a3eaf7ebede99c
hendercine <james@hendercine.com>
2018-08-26 20:14:20 -0700

    feat($mainview): setup for tablets

--- app/src/main/java/com/hendercine/android/gifavorites/view/mainview/MainActivity.java
+++ app/src/main/java/com/hendercine/android/gifavorites/view/mainview/MainActivity.java
@@ -8,13 +8,21 @@
 
 package com.hendercine.android.gifavorites.view.mainview;
 
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.RecoverySystem;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 import com.hendercine.android.gifavorites.R;
 import com.hendercine.android.gifavorites.view.base.BaseActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
 import timber.log.Timber;
 
 /**
@@ -22,14 +30,42 @@ import timber.log.Timber;
  */
 public class MainActivity extends BaseActivity {
 
+    private static final String POSITION_STATE_KEY = "scroll_position";
+    private MainGridRecyclerAdapter mAdapter;
+    private Subscription subscription;
+    private CompositeSubscription mCompositeSubscription;
+    private ConnectivityManager mConnectivityManager;
+    private RecyclerView mRecyclerView;
+    private GridLayoutManager mGridLayoutManager;
+    private RecoverySystem.ProgressListener mListener;
+    private MenuItem mActionProgressItem;
+    private int mScrollPosition;
+    private boolean mIsTablet;
+
+    @Nullable
+    @BindView(R.id.main_grid_recycler)
+    RecyclerView mMainGridRecycler;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         Timber.tag("LifeCycles");
         Timber.d("In onCreate");
 
+        mConnectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        mIsTablet = getResources().getBoolean(R.bool.isTablet);
 
+        int spanCount;
+        // Set the spanCount for the view depending on device type.
+        if (mIsTablet) {
+            mRecyclerView = tabletGridCards;
+            spanCount = 3;
+        } else {
+            mRecyclerView = handHeldGridCards;
+            spanCount = 1;
+        }
     }
 }
