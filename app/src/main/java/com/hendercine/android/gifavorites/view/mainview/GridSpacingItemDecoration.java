2b84e148c4a0fcbc6e4397d73f1e7d17236d42d0
hendercine <james@hendercine.com>
2018-08-26 23:07:51 -0700

    feat($mainview): add ItemDecoration

---
+++ app/src/main/java/com/hendercine/android/gifavorites/view/mainview/GridSpacingItemDecoration.java
@@ -0,0 +1,50 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 8/26/18 9:27 PM
+ */
+
+package com.hendercine.android.gifavorites.view.mainview;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+/**
+ * Gifavorites created by artemis on 8/26/18.
+ */
+class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+    private int spanCount;
+    private int spacing;
+    private boolean includeEdge;
+
+    public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+        this.spanCount = spanCount;
+        this.spacing = spacing;
+        this.includeEdge = includeEdge;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        int position = parent.getChildLayoutPosition(view);
+        int column = position % spanCount;
+
+        if (includeEdge) {
+            outRect.left = spacing - column * spacing / spanCount;
+            outRect.right = (column + 1) * spacing / spanCount;
+
+            if (position < spanCount) {
+                outRect.top = spacing;
+            }
+            outRect.bottom = spacing;
+        } else {
+            outRect.left = column * spacing / spanCount;
+            outRect.right = spacing - (column + 1) * spacing / spanCount;
+            if (position >= spanCount) {
+                outRect.top = spacing;
+            }
+        }
+    }
+}
